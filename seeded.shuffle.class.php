f579c7e1cac1c4ceb50cf1ed4deb27d25faa4c6a
jakemadness <j4kem4dness@live.com>
2018-04-18 11:30:56 +0100

    Create seeded.shuffle.class.php

---
+++ seeded.shuffle.class.php
@@ -0,0 +1,50 @@
+<?php
+/*
+   @https://github.com/jakemadness/fisher-yates-seeded-shuffle
+*/
+class seeded_shuffle {
+
+
+   public static function data_length($data) {
+      return (is_array($data) ? count($data) : strlen($data));
+   }
+
+
+   public static function seed($data) {
+      $seed   = '';
+      $length = self::data_length($data);
+      for ( $i = 0; $i < $length; ++$i ) {
+         $seed .= mt_rand(0, 9);
+      }
+      return $seed;
+   }
+
+
+   public static function shuffle($data, $seed) {
+      $length1 = self::data_length($data);
+      $length2 = self::data_length($seed);
+      for ( $i = 0; $i < $length1; ++$i ) {
+         $swap        = $seed[$i % $length2] % $length1;
+         $temp        = $data[$swap];
+         $data[$swap] = $data[$i];
+         $data[$i]    = $temp;
+      }
+      return $data;
+   }
+
+
+   public static function unshuffle($data, $seed) {
+      $length1 = self::data_length($data);
+      $length2 = self::data_length($seed);
+      for ( $i = $length1 - 1; $i >= 0; --$i ) {
+         $swap        = $seed[$i % $length2] % $length1;
+         $temp        = $data[$swap];
+         $data[$swap] = $data[$i];
+         $data[$i]    = $temp;
+      }
+      return $data;
+   }
+
+
+}
+?>
